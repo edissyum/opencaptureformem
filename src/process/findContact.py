38b641c6e2b5b98415b619d6ca6c3ff7a02153e5
Nathan Cheval <nathan.cheval@outlook.fr>
2019-04-11 11:30:02 +0200

    update

--- src/process/findContact.py
+++ src/process/findContact.py
@@ -1,3 +1,20 @@
+# This file is part of OpenCapture.
+
+# OpenCapture is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# OpenCapture is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with OpenCapture.  If not, see <https://www.gnu.org/licenses/>.
+
+# @dev : Nathan Cheval <nathan.cheval@outlook.fr>
+
 import re
 from threading import Thread
 
