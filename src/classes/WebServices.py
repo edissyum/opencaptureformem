4ccc768846be7077e3e2e93558d9556332280e7a
Nathan <nathan.cheval@outlook.fr>
2020-11-03 17:51:13 +0100

    Improve mail Attachment return for log

--- src/classes/WebServices.py
+++ src/classes/WebServices.py
@@ -273,9 +273,9 @@ class WebServices:
 
             if res.status_code != 200:
                 self.Log.error('(' + str(res.status_code) + ') MailInsertAttachmentsIntoMaarchError : ' + str(res.text))
-                return False
+                return False, json.loads(res.text)
             else:
-                return res.text
+                return True, json.loads(res.text)
         except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as e:
             self.Log.error('Error while inserting in Maarch')
             self.Log.error('More information : ' + str(e))
