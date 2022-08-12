ba6916477de87f222bfc6f1c74af0d3c37c0f947
Nathan <nathan.cheval@outlook.fr>
2022-08-12 10:57:59 +0200

    Add option to capture chrono number and link to this document

---
+++ src/process/FindChrono.py
@@ -0,0 +1,37 @@
+# This file is part of Open-Capture.
+
+# Open-Capture is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Open-Capture is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Open-Capture.  If not, see <https://www.gnu.org/licenses/>.
+
+# @dev : Nathan Cheval <nathan.cheval@outlook.fr>
+
+import re
+from threading import Thread
+
+
+class FindChrono(Thread):
+    def __init__(self, text, process, log):
+        Thread.__init__(self, name='chronoThread')
+        self.Log = log
+        self.text = text
+        self.chrono = None
+        self.process = process
+
+    def run(self):
+        """
+        Override the default run function of threading package
+        This will search for a chrono number into the text of original PDF
+
+        """
+        for _chrono in re.finditer(r"" + self.process['chronoregex'] + "", self.text):
+            self.chrono = _chrono.group()
