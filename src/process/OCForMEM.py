fd557df4ea21a560fa02e5d0635e653ec9743972
Nathan Cheval <nathan.cheval@outlook.fr>
2024-06-11 10:40:19 +0200

    Improve e-rec

--- src/process/OCForMEM.py
+++ src/process/OCForMEM.py
@@ -472,27 +472,26 @@ def process(args, file, log, separator, config, image, ocr, locale, web_service,
 
                 for barcode in detected_barcodes:
                     if barcode.type == reconciliation_type:
-                        log.info(f"Detected barcode data: {barcode.data.decode('utf-8')}")
+                        log.info(f"Detected barcode data : {barcode.data.decode('utf-8')}")
                         response = web_service.check_attachment(barcode.data.decode('utf-8'))
-                        if isinstance(response, dict):
+                        if isinstance(response, dict) and response[0]:
                             chrono = barcode.data.decode('utf-8')
-                            log.info('OK')
-                        elif isinstance(response, tuple):
-                            if not response[0]:
-                                chrono = ''
-                                log.info('KO')
-                                log.error(f"Error response: {response[1]}")
+                        elif isinstance(response, tuple) or not response[0]:
+                            chrono = ''
+                            log.error(f"Error response: {response[1]}")
 
             if chrono != '':
-                log.info('Insert attachment reconciliation')
-                res = web_service.insert_attachment_reconciliation(file_to_send, chrono, config_mail.cfg[_process]['processreconciliationsuccess'], config)
+                log.info('Start RECONCILIATION process')
+                res = web_service.insert_attachment_reconciliation(file_to_send, chrono,
+                                                                   'OCForMEM_reconciliation_found', config)
             else:
-                log.info('Insert letterbox from mail')
+                log.info('Start DEFAULT process')
                 args['data']['file'] = file
                 res = web_service.insert_letterbox_from_mail(args['data'], config_mail.cfg[_process])
         else:
             log.info('Insert letterbox from mail default')
             res = web_service.insert_letterbox_from_mail(args['data'], config_mail.cfg[_process])
+
         if res:
             log.info('Insert email OK : ' + str(res))
             if chrono_number:
