612d6598bc28db269da3a2736a4e9222b18636d5
arthur-mdn <arthur@mondon.pro>
2024-07-15 14:34:46 +0200

    Script to regenerate secret_key by asking which custom_id to edit

--- scripts/regenerate_secret_key.sh
+++ scripts/regenerate_secret_key.sh
@@ -18,9 +18,24 @@
 # @dev : Arthur Mondon <arthur@mondon.pro>
 
 defaultPath=/opt/edissyum/opencaptureformem/
+config_list_file="$defaultPath/src/config/custom.json"
 
-echo "Warning : Every users sessions and tokens will be invalidated if you edit the secret_key !"
-echo "If you want to continue, please write the following sentence : 'I confirm the secret_key regeneration'."
+echo "Available custom_ids:"
+jq -r '.[].custom_id' $config_list_file
+
+while true; do
+    echo "Please enter a valid custom_id from the above list:"
+    read -r custom_id
+
+    if jq -e --arg cid "$custom_id" '.[] | select(.custom_id == $cid)' $config_list_file > /dev/null; then
+        break
+    else
+        echo "Invalid custom_id. Try again."
+    fi
+done
+
+echo "Warning : Every users sessions and tokens will be invalidated if you edit the secret_key!"
+echo "If you want to continue, please write the following sentence: 'I confirm the secret_key regeneration'."
 
 read -r confirmation
 
@@ -30,7 +45,8 @@ if [ "$confirmation" != "I confirm the secret_key regeneration" ]; then
 fi
 
 secret=$(python3 -c 'import secrets; print(secrets.token_hex(32))')
-config_file="$defaultPath/src/config/config.ini"
+config_file=$(jq -r --arg cid "$custom_id" '.[] | select(.custom_id == $cid) | .config_file_path' $config_list_file)
+
 if grep -q 'secret_key' "$config_file"; then
     sed -i "s/^secret_key.*/secret_key = $secret/" "$config_file"
 else
@@ -41,5 +57,3 @@ chown "$user":"$group" "$config_file"
 chmod 600 "$config_file"
 
 echo "The secret_key has been regenerated in the configuration file"
-
-echo "The secret_key file has been regenerated"
