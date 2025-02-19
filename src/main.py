e4530829c274ea5725f339f56f8bd413d24c8980
Nathan Cheval <nathan.cheval@outlook.fr>
2025-02-19 10:48:00 +0100

    Update default config file

--- src/main.py
+++ src/main.py
@@ -152,7 +152,7 @@ def process_file(image, path, config, log, args, separator, ocr, locale, web_ser
             return res
 
 
-@OCForMEM.task()
+# @OCForMEM.task()
 def launch(args):
     start = time.time()
     # Init all the necessary classes
