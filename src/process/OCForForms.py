e0a7929e2aa289adab64ce4cffe96f8dd89952cd
Nathan Cheval <nathan.cheval@outlook.fr>
2025-03-04 09:22:01 +0100

    Improve form process

--- src/process/OCForForms.py
+++ src/process/OCForForms.py
@@ -226,14 +226,16 @@ def process_form(args, config, config_mail, log, web_service, process_name, file
                                             args['data']['customFields'].update({mapping[cpt]['column']: text_data})
                                     else:
                                         args['data'][column] = text_data
-                res_contact = web_service.retrieve_contact_by_mail(results[contact_table]['email'])
-                if res_contact:
-                    log.info('Contact found using email : ' + results[contact_table]['email'])
-                    args['data']['senders'] = [{'id': res_contact['id'], 'type': 'contact'}]
-                else:
-                    res_contact = web_service.create_contact(results[contact_table])
-                    if res_contact[0]:
-                        args['data']['senders'] = [{'id': res_contact[1]['id'], 'type': 'contact'}]
+
+                if 'email' in results[contact_table]:
+                    res_contact = web_service.retrieve_contact_by_mail(results[contact_table]['email'])
+                    if res_contact:
+                        log.info('Contact found using email : ' + results[contact_table]['email'])
+                        args['data']['senders'] = [{'id': res_contact['id'], 'type': 'contact'}]
+                    else:
+                        res_contact = web_service.create_contact(results[contact_table])
+                        if res_contact[0]:
+                            args['data']['senders'] = [{'id': res_contact[1]['id'], 'type': 'contact'}]
 
                 res = web_service.insert_letterbox_from_mail(args['data'], config_mail.cfg[process_name])
                 if res:
